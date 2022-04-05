5de3f976e042955bd88089f9391c132b35c783b2
LucasFFuchs <102075600+LucasFFuchs@users.noreply.github.com>
2022-04-04 21:35:54 -0300

    Add files via upload

---
+++ Tema 2 - Login e senha.py
@@ -0,0 +1,10 @@
+Usuário=str(input("Insira seu nome de usuário "))
+Senha=str(input("Insira sua senha "))
+
+if (Usuário=="Lucas Fuchs"):
+    if (Senha=="123"):
+        print("Login correto")
+    else:
+        print("Login falhou")
+else:
+    print("Usuário não existe")
